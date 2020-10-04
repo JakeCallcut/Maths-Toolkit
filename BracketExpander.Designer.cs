f56597637b40085485f7d4b0b2f07101ad26065d
Jake Callcut <jakecallcut123@gmail.com>
2020-10-04 14:31:27 +0100

    bracket expander
    added groundwork and formatting

--- BracketExpander.Designer.cs
+++ BracketExpander.Designer.cs
@@ -29,20 +29,51 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BracketExpander));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnMenu = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.Color.Gainsboro;
+            this.lblTitle.Location = new System.Drawing.Point(277, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(401, 55);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Bracket Expander";
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMenu.Location = new System.Drawing.Point(214, 365);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(232, 73);
+            this.btnMenu.TabIndex = 1;
+            this.btnMenu.Text = "Back To Menu";
+            this.btnMenu.UseVisualStyleBackColor = true;
             // 
             // BracketExpander
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.ClientSize = new System.Drawing.Size(977, 450);
+            this.Controls.Add(this.btnMenu);
+            this.Controls.Add(this.lblTitle);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "BracketExpander";
             this.Text = "Bracket Expander";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnMenu;
     }
 }
